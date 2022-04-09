f7a01636c48ab358c46f813a31e18ce1e04dbf48
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-09 12:52:18 +0600

    #2 Добавил роут для детального просмотра

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from gallery.views import PhotoListView
+from gallery.views import PhotoListView, PhotoDetailView
 
 app_name = 'gallery'
 
 urlpatterns = [
     path('', PhotoListView.as_view(), name='photo_list'),
-    # path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('product/<int:pk>', PhotoDetailView.as_view(), name='photo_detail'),
     # path('product/add', ProductCreateView.as_view(), name='product_create'),
     # path('product/<int:pk>/edit', ProductUpdateView.as_view(), name='product_edit'),
     # path('product/<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete'),
