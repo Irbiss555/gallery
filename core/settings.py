578a3c872e02abfbb5fa592ff7cc18b6f5fdbf6b
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-09 13:26:45 +0600

    #2 Добавил настройки редиректа при входе и выходе

--- core/settings.py
+++ core/settings.py
@@ -129,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'gallery:photo_list'
+LOGOUT_REDIRECT_URL = 'gallery:photo_list'
