289b1b209390bd14e1c7438c6dab8849f0ae88bc
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-09 11:56:17 +0600

    #1 Добавил роут для регистрации

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from django.contrib.auth.views import LogoutView
 
-from accounts.views import LoginUserView
+from accounts.views import LoginUserView, RegisterView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='user_create'),
 ]
