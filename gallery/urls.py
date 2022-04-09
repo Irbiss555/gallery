cc1d2fc2fbbf0b2d2f41cc04a0b86e8f2b1b3a3c
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-09 14:15:22 +0600

    #2 Добавил роут для удаления фото

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from gallery.views import PhotoListView, PhotoDetailView, PhotoCreateView, PhotoUpdateView
+from gallery.views import (
+    PhotoListView, PhotoDetailView,
+    PhotoCreateView, PhotoUpdateView,
+    PhotoDeleteView
+)
 
 app_name = 'gallery'
 
@@ -8,5 +12,5 @@ urlpatterns = [
     path('photo/<int:pk>', PhotoDetailView.as_view(), name='photo_detail'),
     path('photo/add', PhotoCreateView.as_view(), name='photo_create'),
     path('photo/<int:pk>/edit', PhotoUpdateView.as_view(), name='photo_edit'),
-    # path('photo/<int:pk>/delete', PhotoDeleteView.as_view(), name='photo_delete'),
+    path('photo/<int:pk>/delete', PhotoDeleteView.as_view(), name='photo_delete'),
 ]
