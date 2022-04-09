d0d5eae85140997b14a80a1345d14e21878ae6cf
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-09 13:57:27 +0600

    #2 Добавил роут для редактирования фото

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from gallery.views import PhotoListView, PhotoDetailView, PhotoCreateView
+from gallery.views import PhotoListView, PhotoDetailView, PhotoCreateView, PhotoUpdateView
 
 app_name = 'gallery'
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', PhotoListView.as_view(), name='photo_list'),
     path('photo/<int:pk>', PhotoDetailView.as_view(), name='photo_detail'),
     path('photo/add', PhotoCreateView.as_view(), name='photo_create'),
-    # path('photo/<int:pk>/edit', PhotoUpdateView.as_view(), name='photo_edit'),
+    path('photo/<int:pk>/edit', PhotoUpdateView.as_view(), name='photo_edit'),
     # path('photo/<int:pk>/delete', PhotoDeleteView.as_view(), name='photo_delete'),
 ]
