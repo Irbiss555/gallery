2c7753c699ba95fd9f0da90a35aa2646b177a231
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-09 11:56:59 +0600

    #1 Создал представление для регистрации

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from accounts.forms import MyUserCreationForm
 
 
 class LoginUserView(LoginView):
     template_name = 'accounts/registration/login.html'
     # redirect_authenticated_user = True
+
+
+class RegisterView(CreateView):
+    model = get_user_model()
+    template_name = 'accounts/registration/user_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('accounts:login')
