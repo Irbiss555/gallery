42e24480016dd927402312ef0f5b86207176ff6b
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-09 13:27:02 +0600

    #2 Добавил роуты для создания фото

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from gallery.views import PhotoListView, PhotoDetailView
+from gallery.views import PhotoListView, PhotoDetailView, PhotoCreateView
 
 app_name = 'gallery'
 
 urlpatterns = [
     path('', PhotoListView.as_view(), name='photo_list'),
-    path('product/<int:pk>', PhotoDetailView.as_view(), name='photo_detail'),
-    # path('product/add', ProductCreateView.as_view(), name='product_create'),
-    # path('product/<int:pk>/edit', ProductUpdateView.as_view(), name='product_edit'),
-    # path('product/<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete'),
-    # path('product/<int:pk>/review/create/', ProductReviewCreateView.as_view(), name='product_review_create'),
+    path('photo/<int:pk>', PhotoDetailView.as_view(), name='photo_detail'),
+    path('photo/add', PhotoCreateView.as_view(), name='photo_create'),
+    # path('photo/<int:pk>/edit', PhotoUpdateView.as_view(), name='photo_edit'),
+    # path('photo/<int:pk>/delete', PhotoDeleteView.as_view(), name='photo_delete'),
 ]
