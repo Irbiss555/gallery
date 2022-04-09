f1fe9d44e53f6782c3b7d6df8f42554187ffb11c
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-09 11:42:34 +0600

    #1 Создал шаблон для входа пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,4 @@ from django.contrib.auth.views import LoginView
 
 class LoginUserView(LoginView):
     template_name = 'accounts/registration/login.html'
-    redirect_authenticated_user = True
+    # redirect_authenticated_user = True
