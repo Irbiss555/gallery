b566d870f6056663bd46fd5e958fc39832be96d0
Бисен Ирназаров <irnazarov.b.i@gmail.com>
2022-04-09 14:21:27 +0600

    #1 Добавил роут для страницы пользователя

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from django.contrib.auth.views import LogoutView
 
-from accounts.views import LoginUserView, RegisterView
+from accounts.views import LoginUserView, RegisterView, UserDetailView
 
 app_name = 'accounts'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='user_create'),
+    path('user/<int:pk>', UserDetailView.as_view(), name='user_detail'),
 ]
